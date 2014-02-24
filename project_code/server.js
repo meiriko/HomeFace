20f9d741242bcbf77a27ce9b710bcd20a7852dcc
Meir Shahar <meir.shahar@gmail.com>
2014-02-25 01:48:20 +0200

    server cleanup

--- project_code/server.js
+++ project_code/server.js
@@ -1,4 +1,23 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(8080);
+var express = require('express');
+var app = express();
+var requestSVC = require('request');
+
+app.get(/^\/pt\/(.*)/, function(req, res){
+	// res.end("Hello, " + req.params[0] + ".");
+	var ptUrl = req.params[0]
+	if( ptUrl.indexOf('http') !== 0 ) {
+		ptUrl = 'http://' + ptUrl;
+	}
+	// res.end('looking for: ' + ptUrl);
+	try {
+		requestSVC(ptUrl).pipe(res);
+	} catch( e ) {
+		console.log( e );
+	}
+});
+app.get('/', function(req, res){
+	res.redirect('examples/homeface_tester.html');
+});
+app.use(express.static(__dirname));
+
+app.listen(process.env.PORT || 8080);
